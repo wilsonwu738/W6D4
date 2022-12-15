f171aaee83c9febbdfcefca259dbd966d4f762e5
Wilson Wu <wuyecheng738@gmail.com>
2022-12-15 10:28:28 -0500

    finsihed routes

--- routes_and_controllers/app/controllers/users_controller.rb
+++ routes_and_controllers/app/controllers/users_controller.rb
@@ -5,18 +5,41 @@ class UsersController < ApplicationController
     render json: @user
   end
 
-def create
-  user = User.new(params.require(:user).permit(:name, :email))
-  if user.save
-    render json: user
-  else
-    render json: user.errors.full_messages, status: :unprocessable_entity
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+
   end
-end
+
 
   def show
-    render json: params
+    render json: User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: user
   end
 
   
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
 end
